98eb5f272b0281752f5f81a802fe004bbed3a9c8
seojiaf <dhlavaty.jr@gmail.com>
2026-06-28 13:21:37 +0200

    Initial commit

---
+++ index.py
@@ -0,0 +1,51 @@
+from operator import imod
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+
+from pygame.constants import QUIT
+
+pygame.init()
+
+pygame.display.set_caption("Pygame Project")
+
+
+BG_COLOR = (0, 0, 0)
+WIDHT, HEIGHT = 800, 600
+
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDHT, HEIGHT))
+
+def get_background(name):
+    image = pygame.image.load(join("assets","Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDHT // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i * width, j * height]
+            tiles
+
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
+
+
+if __name__ == "__main__":
+    main(window)
